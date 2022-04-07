4408bf090808f7c3c1df2f548882dc75cf1bc5bb
Brice Venayre <brice.venayre@gmail.com>
2022-04-07 17:57:30 +0200

    add model/turn.py

---
+++ model/turn.py
@@ -0,0 +1,12 @@
+"""Turn model."""
+
+
+class Turn:
+    """Class managing turns."""
+
+    def __init__(self, name: str, match: list(object), start_time, end_time):
+        """Initialize turn objects."""
+        self.name = name
+        self.match = ()
+        self.start_time = start_time
+        self.end_time = end_time
