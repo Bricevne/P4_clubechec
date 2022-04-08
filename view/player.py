3bd2246fdfa73beb66558d064ed2e6da8847a747
Brice Venayre <brice.venayre@gmail.com>
2022-04-08 16:48:42 +0200

    add view player.py

---
+++ view/player.py
@@ -0,0 +1,30 @@
+"""Player view."""
+
+
+class PlayerView:
+    """Class managing players views."""
+
+    @staticmethod
+    def get_name():
+        """Display name."""
+        return "Name: "
+
+    @staticmethod
+    def get_surname():
+        """Display surname."""
+        return "Surname: "
+
+    @staticmethod
+    def get_birthdate():
+        """Display birthdate."""
+        return "Birthdate: "
+
+    @staticmethod
+    def get_gender():
+        """Display gender."""
+        return "Gender: "
+
+    @staticmethod
+    def get_rank():
+        """Display rank."""
+        return "Rank: "
