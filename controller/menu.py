7f8a0a28051f5a6538c87a82977ce632de2a2907
Brice Venayre <brice.venayre@gmail.com>
2022-04-08 16:49:51 +0200

    controller menu

--- controller/menu.py
+++ controller/menu.py
@@ -14,9 +14,9 @@ class MenuManager:
     def select_menu_option(self):
         """Ask for User choice."""
         menu_option = 0
-        while menu_option not in (1, 2, 3):
+        while menu_option not in (1, 2, 3, 4):
             try:
                 menu_option = int(input(self.display.get_menu()))
-            except (TypeError, ValueError):
+            except ValueError:
                 self.display.get_wrong_choice()
         return menu_option
