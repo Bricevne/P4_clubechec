f10b4f03d5fbee99cef7f3d3adb784192e74f42b
Brice Venayre <brice.venayre@gmail.com>
2022-04-08 16:49:18 +0200

    update view menu

--- view/menu.py
+++ view/menu.py
@@ -7,11 +7,15 @@ class MenuView:
     @staticmethod
     def get_menu():
         """Display menu."""
-        menu_options = "1. Create tournament \n2. Add player \n3. View ranking \n"
+        menu_options = "Welcome to the event! Please choose an option.\n\
+        1. Create tournament\n\
+        2. Add player\n\
+        3. View ranking\n\
+        4. Quit\n"
         return menu_options
 
     @staticmethod
     def get_wrong_choice():
         """Display error message."""
-        error_message = "Oops!  That was no valid option.  Try again."
-        return error_message
+        error_message = "Oops! That was no valid option. Try again."
+        return print(error_message)
